39d28279c3ed0fbb82bac191911e95916e30b314
rrajeshwaran <rjrajeshjgn@gmail.com>
2022-03-14 21:01:17 +0530

    new message

--- src/org/com/Client.java
+++ src/org/com/Client.java
@@ -8,13 +8,14 @@ System.out.println("678");
 	}
 	private void cliName() {
 System.out.println("HCL");
+System.out.println("hcl1");
 	}
 	public static void main(String[] args) {
 		Client c = new Client();
 		c.cliId();
 		c.cliName();
 		Employees e = new Employees();
-	   e.empId();
+		e.empId();
 		e.empName();
 			}
 
